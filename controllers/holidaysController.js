c8958eac18470601ae9a7e157573f38c9d796bf2
justsaykk <kaikein.woo@gmail.com>
2022-04-02 11:03:54 +0800

    seeded some data

--- controllers/holidaysController.js
+++ controllers/holidaysController.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const Holiday = require("../models/model_holidays");
 const router = express.Router();
 
 // Home page route.
 router.get("/", function (req, res) {
   res.send("Wiki home page");
+});
+
+router.get("/seed", async (req, res) => {
+  await Holiday.deleteMany({});
+  await Holiday.insertMany([
+    {
+      name: "New Year's Day",
+    },
+    {
+      name: "Good Friday",
+    },
+  ]);
+  res.send("holidays seeded");
 });
 
 module.exports = router;
